1e823d00092c5a9a8a25d7e1025c326939b5687a
Venkat <wnsadmi@gmail.com>
2023-12-19 20:26:30 +0530

    my pro

---
+++ src/main/java/boa/App.java
@@ -0,0 +1,13 @@
+package boa;
+
+/**
+ * Hello world Good day!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
